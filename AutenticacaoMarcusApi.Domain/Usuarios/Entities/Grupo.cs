c9511dcaf96f5fb948275765a186e96165f044b9
marcusruas <marcusruas.andrade@gmail.com>
2024-01-31 00:20:25 -0300

    Adicionado estrutura inicial do projeto

---
+++ AutenticacaoMarcusApi.Domain/Usuarios/Entities/Grupo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutenticacaoMarcusApi.Domain.Grupos.Entities
+{
+    public class Grupo
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
