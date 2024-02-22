c58db3bc57ad65c8a3723af0921c2451ecdcb2e6
marcusruas <marcusruas.andrade@gmail.com>
2024-02-22 01:30:16 -0300

    Fixed data annotation

--- UserManagement.Features/Roles/Requests/CreateRole/CreateRoleRequest.cs
+++ UserManagement.Features/Roles/Requests/CreateRole/CreateRoleRequest.cs
@@ -11,7 +11,7 @@ namespace UserManagement.Features.Roles.Requests.CreateRole
 {
     public class CreateRoleRequest : IRequest<bool>
     {
-        [Required(ErrorMessage = "The role description is required.")]
+        [Required(ErrorMessage = "The role name is required.")]
         [MaxLength(80, ErrorMessage = "The role name must contain a maximum of 80 characters.")]
         public string? Name { get; set; }
         [MaxLength(200, ErrorMessage = "The role description must contain a maximum of 200 characters.")]
